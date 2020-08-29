7ea13da1722672df1d8f4048ff6af5872b0c7b0a
Mridhula Senthil Kumar <s.mridhula@outlook.com>
2020-08-29 14:33:53 -0700

    Learning Variablesgit

---
+++ add.py
@@ -0,0 +1,4 @@
+num1 = int(input ('enter a number: '))
+num2 = int(input ('enter a second number: '))
+total = num1 + num2
+print ("Total of {} + {} = {}".format(num1,num2,total))
---
+++ helloworld.py
@@ -0,0 +1,1 @@
+print ("MRIDHULA IS AWESOME!") 
