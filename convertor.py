a4afb9379d5b300bf3e6612578084165bfb55557
Mridhula Senthil Kumar <s.mridhula@outlook.com>
2020-09-05 15:36:20 -0700

    added converstion program

---
+++ convertor.py
@@ -0,0 +1,4 @@
+usd = float(input ('Enter the amount to convert (USD): '))
+cr = float(input ('Enter the conversion rate: '))
+converted_amount = usd * cr
+print ("USD {} with converstation rate {} = {}".format(usd,cr,converted_amount))
